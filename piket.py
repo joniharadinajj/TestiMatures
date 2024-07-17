abcfb74fa186ae97408e24103410932eed519bf8
JONI <77753643+joniharadinajj@users.noreply.github.com>
2024-07-17 11:16:08 +0200

    Add files via upload

---
+++ piket.py
@@ -0,0 +1,11 @@
+piket = {
+    "Dea" : 77,
+    "Ereni": 81,
+    "Ela" : 75,
+    "Trimi" : 90,
+    "Rita": 100
+}
+shuma = sum(piket.values())
+print(f"Shuma e pikeve eshte: {shuma}")
+mesatarja = shuma / len(piket)
+print(f"Piket mesatare jane: {mesatarja}")
